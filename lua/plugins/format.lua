c2a99d400597deb8eb6a81692677dee44edc0884
Mac Deluca <Mac.Deluca@quartech.com>
2024-03-11 20:21:30 -0700

    updated neovim to 0.9.5

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,20 +1,20 @@
 return {
-  "stevearc/conform.nvim",
-  event = 'InsertEnter',
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "black", "isort" },
-      javascript = { { "prettierd", "prettier" } },
-      typescript = { { "prettierd", "prettier" } },
-      javascriptreact = { { "prettierd", "prettier" } },
-      typescriptreact = { { "prettierd", "prettier" } },
-      bash = { "shellcheck" },
-    },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-    notify_on_error = false,
-  },
+	"stevearc/conform.nvim",
+	event = "InsertEnter",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "black", "isort" },
+			--javascript = { { "prettierd", "prettier" } },
+			--typescript = { { "prettierd", "prettier" } },
+			--javascriptreact = { { "prettierd", "prettier" } },
+			--typescriptreact = { { "prettierd", "prettier" } },
+			bash = { "shellcheck" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+		notify_on_error = false,
+	},
 }
